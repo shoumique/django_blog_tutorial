a0520eea5a57df8756765b37c36022feec11b16e
Md. Shoumique Hasan <shoumique.hasan@bjitgroup.com>
2023-03-28 15:16:44 +0600

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('', views.post_list, name='post_list')
 ]
